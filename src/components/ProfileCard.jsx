e64cb579f684533bd9aa1028f8b099e7bfe4c06e
Marwa Qadeer <Marwaqadeer2@gmail.com>
2026-07-11 21:14:25 +0430

    Add birthday countdown

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,6 +3,34 @@ function ProfileCard({
     deleteProfile
 }) {
 
+    const calculateDaysLeft = () => {
+
+        const today = new Date();
+
+        const birthday = new Date(profile.birthday);
+
+        const nextBirthday = new Date(
+            today.getFullYear(),
+            birthday.getMonth(),
+            birthday.getDate()
+        );
+
+        if (nextBirthday < today) {
+
+            nextBirthday.setFullYear(
+                today.getFullYear() + 1
+            );
+        }
+
+        const difference = 
+           nextBirthday - today;
+
+        return Math.ceil(
+            difference / 
+            (1000 * 60 * 60 * 24)
+        );
+    };
+
     return(
 
         <div className="bg-white rounded-2xl shadow-md p-5 mt-6">
@@ -17,6 +45,10 @@ function ProfileCard({
 
             <p>
                 ❤️ Hobbies: {profile.hobbies}
+            </p>
+
+            <p className="mt-2 text-purple-700 font-semibold">
+                ⏳ {calculateDaysLeft()} days until birthday
             </p>
 
             <button className="bg-orange-500 text-white rounded-xl px-4 py-2 mt-4">
